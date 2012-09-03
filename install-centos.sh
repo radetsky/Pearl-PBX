626b044eecfa1f6028220b3380825b5a50c926da
Alex Radetsky <rad@netstyle.com.ua>
2012-09-03 15:09:00 +0300

    install script update

--- install-centos.sh
+++ install-centos.sh
@@ -9,25 +9,25 @@
 #yum install perl-Data-Dumper 
 #yum install httpd httpd-devel httpd-tools 
 
-install -m 755 -o asterisk:asterisk agi-bin/NetSDS-AGI-integration.pl /var/lib/asterisk/agi-bin
-install -m 755 -o asterisk:asterisk agi-bin/NetSDS-route.pl /var/lib/asterisk/agi-bin
+install -m 755 -o asterisk -g asterisk agi-bin/NetSDS-AGI-integration.pl /var/lib/asterisk/agi-bin
+install -m 755 -o asterisk -g asterisk agi-bin/NetSDS-route.pl /var/lib/asterisk/agi-bin
 
-install -m 755 -o asterisk:asterisk bin/* /usr/bin/ 
+install -m 755 -o asterisk -g asterisk bin/* /usr/bin/ 
 
-install -d -m 755 -o asterisk:asterisk /etc/NetSDS
-install -m 644 -o asterisk:asterisk etc/NetSDS/asterisk-router.conf /etc/NetSDS
+install -d -m 755 -o asterisk -g asterisk /etc/NetSDS
+install -m 644 -o asterisk -g asterisk etc/NetSDS/asterisk-router.conf /etc/NetSDS
 ln -sf /etc/NetSDS /etc/PearlPBX
-install -m 644 -o asterisk:asterisk etc/apache2/sites-available/pearlpbx /etc/httpd/conf.d/pearlpbx.conf 
-install -m 755 -o root:root etc/init.d/pearpbx-parsequeuelogd /etc/init.d 
+install -m 644 -o asterisk -g asterisk etc/apache2/sites-available/pearlpbx /etc/httpd/conf.d/pearlpbx.conf 
+install -m 755 -o root -g root etc/init.d/pearpbx-parsequeuelogd /etc/init.d 
 mv -f /etc/asterisk /etc/asterisk.pearlpbx-moved-old-configs-here 
-install -d -m 755 -o asterisk:asterisk /etc/asterisk 
-install -d -m 755 -o asterisk:asterisk etc/asterisk /etc 
+install -d -m 755 -o asterisk -g asterisk /etc/asterisk 
+install -d -m 755 -o asterisk -g asterisk etc/asterisk /etc 
 
-install -m 644 -o root:root lib/*.pm /usr/share/perl5
+install -m 644 -o root -g root lib/*.pm /usr/share/perl5
 cp -a lib/PearlPBX /usr/share/perl5
 cp -a lib/NetSDS /usr/share/perl5
 
-install -m 755 -o asterisk:asterisk sbin/* /usr/sbin/ 
+install -m 755 -o asterisk -g asterisk sbin/* /usr/sbin/ 
 
 mkdir -p /usr/share/pearlpbx
 cp -av share/reports /usr/share/pearlpbx/
