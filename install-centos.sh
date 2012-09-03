f5b6074dcbf03634b2d811b36ca8a26c7d6a3130
Alex Radetsky <rad@netstyle.com.ua>
2012-09-03 14:22:29 +0300

    added skeleton for asterisk configs

--- install-centos.sh
+++ install-centos.sh
@@ -7,9 +7,22 @@
 #rpm -ivh ./rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 # wget rpmforge-release-0.5.2-2.el6.rf.i686.rpm
 #yum install perl-Data-Dumper 
+#yum install httpd httpd-devel httpd-tools 
 
-install -m755 agi-bin/NetSDS-AGI-integration.pl /var/lib/asterisk/agi-bin
-install -m755 agi-bin/NetSDS-route.pl /var/lib/asterisk/agi-bin
+install -m 755 -o asterisk:asterisk agi-bin/NetSDS-AGI-integration.pl /var/lib/asterisk/agi-bin
+install -m 755 -o asterisk:asterisk agi-bin/NetSDS-route.pl /var/lib/asterisk/agi-bin
+install -m 755 -o asterisk:asterisk bin/* /usr/bin/ 
+install -d -m 755 -o asterisk:asterisk /etc/NetSDS
+install -m 644 -o asterisk:asterisk etc/NetSDS/asterisk-router.conf /etc/NetSDS
+ln -sf /etc/NetSDS /etc/PearlPBX
+install -m 644 -o asterisk:asterisk etc/apache2/sites-available/pearlpbx /etc/httpd/conf.d/pearlpbx.conf 
+install -m 755 -o root:root etc/init.d/pearpbx-parsequeuelogd /etc/init.d 
+
+install -m 644 -o root:root lib/*.pm /usr/share/perl5
+cp -a lib/PearlPBX /usr/share/perl5
+cp -a lib/NetSDS /usr/share/perl5
+
+install -m 755 -o asterisk:asterisk sbin/* /usr/sbin/ 
 
 
 
