bf0e36044311c1699fbf6b90044bec5ebfe78c34
Alex Radetsky <rad@rad.kiev.ua>
2013-12-16 17:27:06 +0200

    F1

--- sql/sip_conf.sql
+++ sql/sip_conf.sql
@@ -31,12 +31,16 @@ COPY sip_conf (id, cat_metric, var_metric, commented, filename, category, var_na
 29	0	9	0	sip.conf	general	rtautoclear	yes
 31	0	10	0	sip.conf	general	allowguest	no
 30	0	11	0	sip.conf	general	ignoreregexpire	yes
-34	0	14	0	sip.conf	general	externip	91.212.154.3
+34	0	14	1	sip.conf	general	externip	91.212.154.3
 35	0	15	0	sip.conf	general	localnet	192.168.0.0/24
 33	0	13	0	sip.conf	general	maxexpiry	3600
 32	0	12	0	sip.conf	general	minexpiry	60
 36	0	16	0	sip.conf	general	defaultexpiry	3600
 \.
+
+insert into sip_conf (cat_metric, var_metric, commented, filename, category, var_name, var_val ) values (0,17,0,'sip.conf','general','videosupport','yes');
+
+insert into sip_conf (cat_metric, var_metric, commented, filename, category, var_name, var_val ) values (0,18,0,'sip.conf','general','alwaysauthreject','yes');
 
 -- Completed on 2012-09-04 16:40:58 EEST
 
