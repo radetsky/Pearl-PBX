99bc4f20f8c37e0a701502c256234149c8f30a6d
rad <rad@makerpm.pearlpbx.com>
2015-07-06 17:31:03 +0300

    Адаптируемся дальше под CentOS 7 + Asterisk 13 = PearlPBX 20

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 tar: 
-	cd .. && tar cvf ./PearlPBX-1.4.tar --exclude ./Pearl-PBX/.git ./Pearl-PBX
+	mkdir /tmp/Pearl-PBX-1.4 
+	cp -av * /tmp/Pearl-PBX-1.4
+	cd /tmp && tar cvf ./Pearl-PBX-1.4.tar --exclude ./Pearl-PBX-1.4/.git ./Pearl-PBX-1.4
 
 update_sounds: 
 	cp -av sounds/* /usr/share/asterisk/sounds/
---
+++ bin/PearlPBX-initall.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+first_run_pearlpbx() { 
+
+mkdir -p /var/run/NetSDS 
+ln -sf /etc/NetSDS /etc/PearlPBX
+/usr/bin/postgresql-setup initdb
+cp /var/lib/pgsql/data/pg_hba.conf /tmp/pg_hba.conf 
+cat /tmp/pg_hba.conf | sed 's/ident$/trust/' >/var/lib/pgsql/data/pg_hba.conf
+cp /var/lib/pgsql/data/pg_hba.conf /tmp/pg_hba.conf 
+cat /tmp/pg_hba.conf | sed 's/peer$/trust/' >/var/lib/pgsql/data/pg_hba.conf
+systemctl start postgresql.service
+psql -U postgres -f /etc/NetSDS/sql/create_user_asterisk.sql
+psql -U postgres -f /etc/NetSDS/sql/asterisk.sql
+psql -U asterisk -f /etc/NetSDS/sql/callback.sql
+psql -U asterisk -f /etc/NetSDS/sql/directions_list.sql
+psql -U asterisk -f /etc/NetSDS/sql/directions.sql
+psql -U asterisk -f /etc/NetSDS/sql/sip_conf.sql
+psql -U asterisk -f /etc/NetSDS/sql/extensions_conf.sql
+psql -U asterisk -f /etc/NetSDS/sql/route.sql
+psql -U asterisk -f /etc/NetSDS/sql/local_route.sql
+psql -U asterisk -f /etc/NetSDS/sql/cal.sql
+psql -U asterisk -f /etc/NetSDS/sql/ivr.sql
+mv -f /etc/PearlPBX/asterisk/* /etc/asterisk/
+/usr/sbin/PearlPBX-gui-passwd.pl admin admin
+/usr/bin/ulines.pl
+mkdir /var/www/pearlpbx/files
+chown apache:apache /var/www/pearlpbx/files
+mkdir /usr/share/asterisk/sounds/ru/pearlpbx
+chown apache:apache /usr/share/asterisk/sounds/ru/pearlpbx
+systemctl stop postgresql 
+
+firewall-cmd --permanent  --add-service=http
+firewall-cmd --permanent  --add-port=5060/udp 
+firewall-cmd --permanent  --add-port=5060/tcp 
+firewall-cmd --permanent  --add-port=5061/tcp 
+firewall-cmd --reload 
+
+echo "Installed" >/etc/sysconfig/PearlPBX
+
+}
+
+	if [ -f /etc/sysconfig/PearlPBX ] 
+	then
+		echo OK
+	else 
+		first_run_pearlpbx
+	fi
+	exit 0
+
