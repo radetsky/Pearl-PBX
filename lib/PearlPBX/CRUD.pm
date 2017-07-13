a1f81daa51506818d7b42db68a47b5126831b561
Alex Radetsky <rad@rad.kiev.ua>
2017-07-13 15:54:36 +0300

    initial commit

---
+++ lib/PearlPBX/CRUD.pm
@@ -0,0 +1,30 @@
+package PearlPBX::CRUD;
+
+use warnings; 
+use strict; 
+use feature 'state'; 
+use parent qw (Class::Accessor::Class);
+
+
+# Connect to DB and return object 
+sub new { 
+  my $class = shift; 
+  my $self  = undef; 
+
+  $self = bless $self, $class; 
+  $self->mk_accessors('dbh'); 
+  $self->{db} = PearlPBX::DB->new(); 
+  $self->dbh( $self->{db}->{dbh}); 
+
+  return $self; 
+}
+
+sub paramsToConditionWithAnd { 
+  my ($self, $params) = @_; 
+  my @pairs;
+
+  while ( my ($key, $value) = each %{$params} ) {
+  	push @pairs, "$key=$value"; 
+  }
+  return join(' AND ', @pairs); 
+}
---
+++ lib/PearlPBX/CRUD/Queue.pm
@@ -0,0 +1,85 @@
+package PearlPBX::CRUD::Queue;
+
+use warnings; 
+use strict; 
+
+use parent qw(PearlPBX::CRUD);
+
+=head1 NAME
+
+    PearlPBX::CRUD::Queue - Class for manipulating asterisk queues places in DBI/public.queues;
+
+=head1 SYNOPSIS
+
+    use PearlPBX::CRUD::Queue;
+
+    my $queue = PearlPBX::CRUD::Queue->create(
+        name       => 'testQueue',
+        strategy   => 'ringall',
+        timeout    => 15,
+        maxlen     => 1,
+    );
+
+=head1 DESCRIPTION
+
+
+
+=head1 Methods
+
+=over
+
+=item B<create($options)>
+
+Create a new queue and insert it into DB
+Return hashref { result => 'ok' }
+
+=item B<update($options)>
+
+Update according queue fields in DB with name=$options->name.
+Return hashref { result => 'ok' } or { error => 'Unknown error of confirmation code' }
+
+=item B<read($options)> 
+
+Read queue from DB with options 
+Return hashref { result => 'ok', data => {  name => 'testQueue', 
+										    strategy => 'ringall',
+										    timeout => 15,
+										    maxlen => 5 }};
+
+
+=back
+
+=cut
+
+use constant QUEUES => 'public.queues';
+
+sub create { 
+	my $self   = shift; 
+	my $params = shift; 
+
+}
+
+sub update {
+	my $self   = shift; 
+	my $params = shift; 
+
+}
+
+sub read { 
+	my $self   = shift; 
+	my $params = shift; 
+
+	my $condition = $self->paramsToConditionWithAnd($param);
+	my $sql = "select * from ".QUEUES." where ".$condition; 
+
+	my $sth = $self->dbh->prepare($sql);
+	my $rv = $sth->execute();
+	my $result = $sth->fetchall_hashref('name');
+	return $result; 
+
+}
+
+sub delete {
+	my $params = shift; 
+
+}
