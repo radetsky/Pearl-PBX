708ce976f978b8ff52f39552678b009a8bfd2d6f
Alex Radetsky <rad@rad.kiev.ua>
2017-07-18 15:19:36 +0300

    back compat

--- bin/pickupgroupman.pl
+++ bin/pickupgroupman.pl
@@ -8,11 +8,12 @@
 #===============================================================================
 #
 # Shows and set groups items to pickup feature (*8)
+# Uses old NetSDS::App and fucking old code to back compatibility with 1.3.2 :(
+
 
 use strict;
 use warnings;
 
-use lib "./lib";
 PickupMan->run(
     daemon      => undef,
     verbose     => 1,
@@ -29,10 +30,9 @@ package PickupMan;
 use strict;
 use warnings;
 
-use base qw(PearlPBX::App);
+use base qw(NetSDS::App);
 use Getopt::Long qw(:config auto_version auto_help pass_through);
 use Data::Dumper;
-use PearlPBX::Config -load;
 
 sub start {
   my $self = shift;
@@ -41,6 +41,50 @@ sub start {
 
   $self->SUPER::start();
 
+}
+sub start {
+    my $this = shift;
+
+    $this->mk_accessors('dbh');
+    $this->_db_connect;
+}
+
+sub _db_connect {
+    my $this = shift;
+
+    unless ( defined( $this->{conf}->{'db'}->{'main'}->{'dsn'} ) ) {
+        $this->speak("Can't find \"db main->dsn\" in configuration.");
+        exit(-1);
+    }
+    unless ( defined( $this->{conf}->{'db'}->{'main'}->{'login'} ) ) {
+        $this->speak("Can't find \"db main->login\" in configuraion.");
+        exit(-1);
+    }
+
+    unless ( defined( $this->{conf}->{'db'}->{'main'}->{'password'} ) ) {
+        $this->speak("Can't find \"db main->password\" in configuraion.");
+        exit(-1);
+    }
+
+    my $dsn    = $this->conf->{'db'}->{'main'}->{'dsn'};
+    my $user   = $this->conf->{'db'}->{'main'}->{'login'};
+    my $passwd = $this->conf->{'db'}->{'main'}->{'password'};
+
+    # If DBMS isn' t accessible - try reconnect
+    if ( !$this->dbh or !$this->dbh->ping ) {
+        $this->dbh(
+            DBI->connect_cached( $dsn, $user, $passwd, { RaiseError => 1 } ) );
+    }
+
+    if ( !$this->dbh ) {
+        $this->speak("Cant connect to DBMS!");
+        $this->log( "error", "Cant connect to DBMS!" );
+        exit(-1);
+    }
+    if ( $this->{verbose} ) {
+        $this->agi->verbose( "Database connected.", 3 );
+    }
+    return 1;
 }
 
 sub show_groups {
