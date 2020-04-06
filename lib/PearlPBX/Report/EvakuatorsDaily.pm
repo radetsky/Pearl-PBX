b24f03c3201a2e22cc88473b62d8812c7bc683af
Alex Radetsky <rad@fullstack.center>
2020-04-06 21:44:36 +0300

    second part of new report

---
+++ lib/PearlPBX/Report/EvakuatorsDaily.pm
@@ -0,0 +1,254 @@
+#===============================================================================
+#         FILE:  EvakuatorsDaily.pm
+#  DESCRIPTION:  Returns count of accepted and missed incoming calls for every hour in selected day
+#       AUTHOR:  Alex Radetsky (Rad), <rad@fullstack.center>
+#      COMPANY:  PearlPBX
+#      CREATED:  2020-04-06 ( Quarantine-Quarantine! Drink your whiskey like vaccine )
+#===============================================================================
+package PearlPBX::Report::EvakuatorsDaily;
+use warnings;
+use strict;
+
+use base qw(PearlPBX::Report);
+use Data::Dumper;
+use Template;
+use JSON;
+
+use version; our $VERSION = "1.0";
+our @EXPORT_OK = ();
+
+my @HOURS = (
+    ['00:00:00','00:59:59'],
+    ['01:00:00','01:59:59'],
+    ['02:00:00','02:59:59'],
+    ['03:00:00','03:59:59'],
+    ['04:00:00','04:59:59'],
+    ['05:00:00','05:59:59'],
+    ['06:00:00','06:59:59'],
+    ['07:00:00','07:59:59'],
+    ['08:00:00','08:59:59'],
+    ['09:00:00','09:59:59'],
+    ['10:00:00','10:59:59'],
+    ['11:00:00','11:59:59'],
+    ['12:00:00','12:59:59'],
+    ['13:00:00','13:59:59'],
+    ['14:00:00','14:59:59'],
+    ['15:00:00','15:59:59'],
+    ['16:00:00','16:59:59'],
+    ['17:00:00','17:59:59'],
+    ['18:00:00','18:59:59'],
+    ['19:00:00','19:59:59'],
+    ['20:00:00','20:59:59'],
+    ['21:00:00','21:59:59'],
+    ['22:00:00','22:59:59'],
+    ['23:00:00','23:59:59'],
+);
+
+# foreach my $h (@HOURS) {
+# print $h->[0] . " -> " . $h->[1] . "\n";
+# }
+
+sub new {
+    my ($class, $conf) = @_;
+    my $this = $class->SUPER::new($conf);
+
+    $this->{'connected_with_queue'} = $this->{dbh}->prepare("select count(queue) as s from public.queue_parsed where queue=?
+                           and time between ? and ? and success=1 and callerid not in (select msisdn from ivr.addressbook)");
+
+    $this->{'connected_total'} = $this->{dbh}->prepare("select count(queue) as s from public.queue_parsed where
+                           time between ? and ? and success=1 and callerid not in (select msisdn from ivr.addressbook)");
+
+    $this->{'missed_with_queue'} = $this->{dbh}->prepare("select count(queue) as s from public.queue_parsed where queue=?
+                           and time between ? and ? and success=0 and callerid not in (select msisdn from ivr.addressbook)");
+
+    $this->{'missed_total'} = $this->{dbh}->prepare("select count(queue) as s from public.queue_parsed where
+                           time between ? and ? and success=0 and callerid not in (select msisdn from ivr.addressbook)");
+
+    # Подробный список пропущенных
+    $this->{'missed_list'} = $this->{dbh}->prepare("select time,callerid,holdtime from queue_parsed where queue=?
+                           and success=0 and time between ? and ? order by time desc");
+
+    # Список дозвонившихся после пропущенного
+    $this->{'missed_lucky'} = $this->{dbh}->prepare("select time,holdtime from queue_parsed where queue=?
+                          and callerid=? and success=1 and time between ? and ? order by time desc limit 1");
+
+    # Список обработанных операторами
+    $this->{'missed_done'} = $this->{dbh}->prepare("select calldate, billsec, src from public.cdr where dst=?
+                          and calldate between ? and ? and disposition = 'ANSWERED' order by calldate limit 1");
+
+    bless $this;
+    return $this;
+}
+
+sub _connected {
+    my $this = shift;
+    my $qname = shift;
+    my $since = shift;
+    my $till = shift;
+
+    my $sth_connected;
+    if ($qname ne 'total') {
+        $sth_connected = $this->{'connected_with_queue'}; # prepared statement
+        eval { $sth_connected->execute( $qname, $since, $till); };
+
+    } else {
+        $sth_connected = $this->{'connected_total'}; # prepared statement
+        eval { $sth_connected->execute( $since, $till); };
+    }
+    if ($@) {
+        $this->{error} = $this->{dbh}->errstr;
+        return undef;
+    }
+
+    my $fetch_connected = $sth_connected->fetchrow_hashref;
+    my $connected = $fetch_connected->{'s'}+0;
+
+    return $connected;
+}
+
+sub _missed {
+    my $this  = shift;
+    my $qname = shift;
+    my $since = shift;
+    my $till  = shift;
+
+    my $sth_missed;
+
+    if ($qname ne 'total') {
+        $sth_missed = $this->{'missed_with_queue'};
+        eval { $sth_missed->execute( $qname, $since, $till); };
+    } else {
+        $sth_missed = $this->{'missed_total'};
+        eval { $sth_missed->execute( $since, $till); };
+    }
+    if ($@) {
+        $this->{error} = $this->{dbh}->errstr;
+        return undef;
+    }
+    my $fetch_missed = $sth_missed->fetchrow_hashref;
+    my $missed = $fetch_missed->{'s'}+0;
+    return $missed;
+}
+
+sub _get_lucky_done {
+  my ($this, $queuename, $sincedatetime, $tilldatetime) = @_;
+
+  eval { $this->{'missed_list'}->execute( $queuename, $sincedatetime, $tilldatetime ); };
+  if ($@) {
+      $this->{error} = $this->{dbh}->errstr;
+      return undef;
+  }
+
+  my @first_rows;
+  while ( my $hash_ref = $sth->fetchrow_hashref ) {
+      push @first_rows,$hash_ref;
+  }
+
+  my $till = $this->{'date'} . ' 23:59:59'; # Until the end of the day
+  my $lucky = 0;
+  my $done  = 0;
+
+  foreach my $row (@first_rows) {
+    my $callerid   = $row->{'callerid'};
+    my $first_time = $row->{'time'};
+    unless ( defined ( $this->_lucky($callerid, $first_time, $queuename, $till) ) ) {
+      unless ( defined ( $this->_outtime($callerid, $first_time, $till) ) ) {
+        next;
+      } else {
+        $done++;
+      }
+    } else {
+      $lucky++;
+    }
+  }
+
+  return ($lucky, $done);
+}
+
+sub _outtime {
+    my ($this, $callerid, $first_time, $till) = @_;
+    eval { $this->{'missed_done'}->execute ($callerid, $first_time, $till); };
+    if ($@) {
+        $this->{error} = $this->{dbh}->errstr;
+        return undef;
+    }
+    my $outtime = $sth->fetchrow_hashref;
+    unless ( defined ( $outtime->{'calldate'}) ) { return undef; }
+    return 1;
+}
+
+sub _lucky {
+    my ($this, $callerid, $first_time, $queuename, $tilldatetime) = @_;
+
+    eval { $this->{'missed_lucky'}->execute ($queuename, $callerid, $first_time, $tilldatetime); };
+    if ($@) {
+        $this->{error} = $this->{dbh}->errstr;
+        return undef;
+    }
+    my $lucky = $sth_redial->fetchrow_hashref;
+    unless ( defined ( $lucky->{'time'}) ) { return undef; }
+    return 1;
+}
+
+sub report {
+    my ($this, $params) = @_;
+    my $since = $params->{dateFrom};
+    my $queue = $params->{queue};
+
+    # Save to use later in class
+    $this->{'date'} = $since;
+    my @result;
+    my @graph;
+
+    my $current_hour = 0; # 0 - 23
+    foreach my $h (@HOURS) {
+        my $start = $h->[0];
+        my $stop  = $h->[1];
+
+        my $sincedatetime = $since . " " . $start;
+        my $tilldatetime  = $since . " " . $stop;
+
+        my $connected = $this->_connected($queue, $sincedatetime, $tilldatetime);
+        return undef unless ( defined ( $connected ) );
+        my $missed =    $this->_missed   ($queue, $sincedatetime, $tilldatetime);
+        return undef unless ( defined ( $missed ) );
+        my ($lucky, $done ) = $this->_get_lucky_done ($queue, $sincedatetime, $tilldatetime);
+        my $lost = $missed - $lucky - $done;
+        $lost = 0 if $lost < 0;
+        my $total = $connected + $missed;
+        my $p_lost = ($lost * 100) / $total;
+        my $result_row = {
+            hour => $current_hour,
+            sincedatetime => $sincedatetime,
+            tilldatetime => $tilldatetime,
+            connected => $connected,
+            lost => $lost,
+            total => $total,
+            p_lost => $p_lost
+        };
+        push @result, $result_row;
+        push @graph, {
+            hour => $current_hour,
+            connected => $connected,
+            lost => $lost,
+        };
+
+    }
+
+    my $jdata = encode_json(\@graph);
+
+    my $template = Template->new ( {
+        INCLUDE_PATH => '/usr/share/pearlpbx/reports/templates',
+        INTERPOLATE  => 1,
+    } ) || die "$Template::ERROR";
+
+    my $tmpl_vars = {
+        jdata => $jdata,
+        rawdata => \@result,
+    };
+
+    $template->process('EvakuatorsDaily.html', $tmpl_vars) || die $template->error();
+
+}
+
+
--- lib/PearlPBX/Report/LostInGroups1.pm
+++ lib/PearlPBX/Report/LostInGroups1.pm
@@ -2,14 +2,14 @@
 #
 #         FILE:  LostInGroups.pm
 #
-#  DESCRIPTION:  Returns count of lost calls. 
+#  DESCRIPTION:  Returns count of lost calls.
 #
 #        NOTES:  ---
 #       AUTHOR:  Alex Radetsky (Rad), <rad@rad.kiev.ua>
 #      COMPANY:  PearlPBX
 #      VERSION:  1.0
 #      CREATED:  01.06.2012 05:30:27 EEST
-#     MODIFIED:  27.04.2015 (Remember Olga) 
+#     MODIFIED:  27.04.2015 (Remember Olga)
 #===============================================================================
 
 package PearlPBX::Report::LostInGroups1;
@@ -21,7 +21,7 @@ use warnings;
 use base qw(PearlPBX::Report);
 use Data::Dumper;
 use Template;
-use JSON; 
+use JSON;
 
 use version; our $VERSION = "1.0";
 our @EXPORT_OK = ();
@@ -67,9 +67,9 @@ sub report {
     my $sincedatetime = $this->filldatetime( $params->{'dateFrom'}, $params->{'timeFrom'} );
     my $tilldatetime  = $this->filldatetime( $params->{'dateTo'},  $params->{'timeTo'} );
 
-    my $sql = "select count(queue) as s,queue as queuename from public.queue_parsed 
-      where time between ? and ? and success=0 and callerid not in (select msisdn from ivr.addressbook ) 
-      group by queue order by count(queue) desc"; 
+    my $sql = "select count(queue) as s,queue as queuename from public.queue_parsed
+      where time between ? and ? and success=0 and callerid not in (select msisdn from ivr.addressbook )
+      group by queue order by count(queue) desc";
 
    my $sth = $this->{dbh}->prepare($sql);
     eval { $sth->execute( $sincedatetime, $tilldatetime ); };
@@ -77,54 +77,54 @@ sub report {
         $this->{error} = $this->{dbh}->errstr;
         return undef;
     }
-	
-		my @aOfa; 
-		my @cdr_keys; 
+
+		my @aOfa;
+		my @cdr_keys;
 
     while ( my $hash_ref = $sth->fetchrow_hashref ) {
-			push @aOfa, [ $hash_ref->{'queuename'},$hash_ref->{'s'}+0 ]; 
-			push @cdr_keys, $hash_ref;  
+			push @aOfa, [ $hash_ref->{'queuename'},$hash_ref->{'s'}+0 ];
+			push @cdr_keys, $hash_ref;
     }
 
-    foreach my $pos ( @cdr_keys ) { 
-      my $qname = $pos->{'queuename'}; 
+    foreach my $pos ( @cdr_keys ) {
+      my $qname = $pos->{'queuename'};
       my ( $lucky, $done ) = $this->_get_lucky_done ($qname, $sincedatetime, $tilldatetime);
-      $pos->{'lucky'} = $lucky; 
-      $pos->{'done'}  = $done; 
-      $pos->{'left'}  = $pos->{'s'} - $lucky - $done;  
+      $pos->{'lucky'} = $lucky;
+      $pos->{'done'}  = $done;
+      $pos->{'left'}  = $pos->{'s'} - $lucky - $done;
     }
 
-		my $template = Template->new( { 
+		my $template = Template->new( {
 			INCLUDE_PATH => '/usr/share/pearlpbx/reports/templates',
-			INTERPOLATE  => 1, 
-			} ) || die "$Template::ERROR\n"; 
+			INTERPOLATE  => 1,
+			} ) || die "$Template::ERROR\n";
 
 		my $jdata = encode_json (\@aOfa);
 
-		my $template_vars = { 
+		my $template_vars = {
 			cdr_keys => \@cdr_keys,
-			jdata => $jdata, 
+			jdata => $jdata,
       sincedatetime => $sincedatetime,
       tilldatetime => $tilldatetime,
-		};  
-		$template->process('LostInGroups1.html', $template_vars) || die $template->error(); 
-		
+		};
+		$template->process('LostInGroups1.html', $template_vars) || die $template->error();
+
 }
 
 sub hashref2arrayOfArrays {
 	my ($this,$hash_ref) = @_;
-  my @result; 
+  my @result;
 
-  foreach my $hrkey ( keys %$hash_ref ) { 
-	  my $sub_hashref = $hash_ref->{$hrkey}; 
-		my @sub_array; 
-    foreach my $hrsubkey ( keys %$sub_hashref ) { 
-			push @sub_array, $sub_hashref->{'dst'}; 
-			push @sub_array, $sub_hashref->{'s'}+0; 
-		} 
-		push @result, [ @sub_array ]; 
+  foreach my $hrkey ( keys %$hash_ref ) {
+	  my $sub_hashref = $hash_ref->{$hrkey};
+		my @sub_array;
+    foreach my $hrsubkey ( keys %$sub_hashref ) {
+			push @sub_array, $sub_hashref->{'dst'};
+			push @sub_array, $sub_hashref->{'s'}+0;
+		}
+		push @result, [ @sub_array ];
   }
-	return @result; 
+	return @result;
 }
 
 sub hashref2arrayofhashref {
@@ -139,20 +139,20 @@ sub hashref2arrayofhashref {
   return @output;
 }
 
-sub _get_lucky_done { 
-  my ($this, $queuename, $sincedatetime, $tilldatetime) = @_; 
+sub _get_lucky_done {
+  my ($this, $queuename, $sincedatetime, $tilldatetime) = @_;
 
   # Подробный список пропущенных
-  my $sql = "select time,callerid,holdtime from queue_parsed where queue=? 
-      and success=0 and time between ? and ? order by time desc"; 
+  my $sql = "select time,callerid,holdtime from queue_parsed where queue=?
+      and success=0 and time between ? and ? order by time desc";
 
   # Список дозвонившихся после пропущенного
-  my $sql_redial = "select time,holdtime from queue_parsed where queue=? 
-      and callerid=? and success=1 and time between ? and ? order by time desc limit 1"; 
+  my $sql_redial = "select time,holdtime from queue_parsed where queue=?
+      and callerid=? and success=1 and time between ? and ? order by time desc limit 1";
 
-  # Список обработанных операторами 
-  my $sql_operator_redial = "select calldate, billsec, src from public.cdr where dst=? 
-      and calldate between ? and ? and disposition = 'ANSWERED' order by calldate limit 1"; 
+  # Список обработанных операторами
+  my $sql_operator_redial = "select calldate, billsec, src from public.cdr where dst=?
+      and calldate between ? and ? and disposition = 'ANSWERED' order by calldate limit 1";
 
   my $sth = $this->{dbh}->prepare($sql);
   eval { $sth->execute( $queuename, $sincedatetime, $tilldatetime ); };
@@ -161,56 +161,56 @@ sub _get_lucky_done {
       return undef;
   }
 
-  my @first_rows; 
+  my @first_rows;
   while ( my $hash_ref = $sth->fetchrow_hashref ) {
-      push @first_rows,$hash_ref; 
+      push @first_rows,$hash_ref;
   }
 
-  my $sth_redial  = $this->{dbh}->prepare ($sql_redial); 
-  my $sth_outtime = $this->{dbh}->prepare ($sql_operator_redial); 
+  my $sth_redial  = $this->{dbh}->prepare ($sql_redial);
+  my $sth_outtime = $this->{dbh}->prepare ($sql_operator_redial);
 
   my $lucky = 0;
-  my $done  = 0; 
-  foreach my $row (@first_rows) { 
+  my $done  = 0;
+  foreach my $row (@first_rows) {
     my $callerid = $row->{'callerid'};
-    my $first_time = $row->{'time'}; 
-    unless ( defined ( $this->_lucky($callerid, $first_time, $sth_redial, $queuename, $tilldatetime) ) ) { 
-      unless ( defined ( $this->_outtime($callerid, $first_time, $sth_outtime, $tilldatetime) ) ) { 
+    my $first_time = $row->{'time'};
+    unless ( defined ( $this->_lucky($callerid, $first_time, $sth_redial, $queuename, $tilldatetime) ) ) {
+      unless ( defined ( $this->_outtime($callerid, $first_time, $sth_outtime, $tilldatetime) ) ) {
         next;
       } else {
-        $done++; 
+        $done++;
       }
 
     } else {
-      $lucky++; 
+      $lucky++;
     }
   }
-  return ($lucky, $done); 
+  return ($lucky, $done);
 }
 
-sub _outtime { 
-    my ($this, $callerid, $first_time, $sth, $tilldatetime) = @_; 
-    eval { $sth->execute ($callerid, $first_time, $tilldatetime); }; 
-    if ($@) { 
+sub _outtime {
+    my ($this, $callerid, $first_time, $sth, $tilldatetime) = @_;
+    eval { $sth->execute ($callerid, $first_time, $tilldatetime); };
+    if ($@) {
         $this->{error} = $this->{dbh}->errstr;
-        return undef; 
+        return undef;
     }
     my $outtime = $sth->fetchrow_hashref;
-    unless ( defined ( $outtime->{'calldate'}) ) { return undef; } 
-    return 1;  
+    unless ( defined ( $outtime->{'calldate'}) ) { return undef; }
+    return 1;
 }
 
-sub _lucky { 
-    my ($this, $callerid, $first_time, $sth_redial, $queuename, $tilldatetime) = @_; 
+sub _lucky {
+    my ($this, $callerid, $first_time, $sth_redial, $queuename, $tilldatetime) = @_;
 
-    eval { $sth_redial->execute ($queuename, $callerid, $first_time, $tilldatetime); }; 
-    if ($@) { 
+    eval { $sth_redial->execute ($queuename, $callerid, $first_time, $tilldatetime); };
+    if ($@) {
         $this->{error} = $this->{dbh}->errstr;
-        return undef; 
+        return undef;
     }
     my $lucky = $sth_redial->fetchrow_hashref;
     unless ( defined ( $lucky->{'time'}) ) { return undef; }
-    return 1;  
+    return 1;
 }
 
 1;
